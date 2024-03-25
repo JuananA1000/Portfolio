389956867cc1bfc2f2c10b96496453e6170abfe9
Juanan <juanantonioamillopez@gmail.com>
2024-03-25 13:55:19 +0100

    SAVE

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+
+import './Header.css'
+
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <a href='/home'>Experiencia</a>
+        <a href='/projects'>Proyectos</a>
+        <a href='/about'>Sobre mí</a>
+        <a href='/contact'>Contacto</a>
+        <button>n</button>
+      </nav>
+    </header>
+  );
+}
