090afb644f1540c9718d0b3a13ab894a4d7a02eb
Juanan <juanantonioamillopez@gmail.com>
2024-03-26 12:02:33 +0100

    Eliminar estilos por defecto

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,8 +2,6 @@ import Header from '../components/Header/Header';
 import Descripcion from '../components/Descripcion/Descripcion';
 import Experiencia from '../components/Experiencia/Experiencia';
 
-import './App.css';
-
 function App() {
   return (
     <>
