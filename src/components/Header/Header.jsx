077ab1030e1a14ef2e877fdc05147ec84b72ce22
Juanan <juanantonioamillopez@gmail.com>
2024-03-27 12:07:39 +0100

    Previsión de modo oscuro

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,18 @@
 import translate from '../../es.json';
 
-import './Header.css'
+import './Header.css';
 
 export default function Header() {
+  const modoNoche = () => console.log('este botón será para cuando se cree el modo oscuro');
+
   return (
     <header>
-      <nav className="header-nav">
+      <nav className='header-nav'>
         <a href='/home'>{translate['header-home']}</a>
         <a href='/projects'>{translate['header-projects']}</a>
         <a href='/about'>{translate['header-about']}</a>
         <a href='/contact'>{translate['header-contact']}</a>
-        <button>n</button>
+        <button onClick={modoNoche}>n</button>
       </nav>
     </header>
   );
