b7b148150ea2a161f205c06532fb3f9a8baab383
Juanan <juanantonioamillopez@gmail.com>
2024-04-01 14:22:34 +0200

    Añadir descripción en "Sobre mí"

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -10,7 +10,31 @@ export default function About() {
         <img src={about} alt='about.svg' id='about-img' />
         <h2>{translate['about-h2']}</h2>
       </div>
-      About
+
+      <p>
+        Me llamo Juan Antonio pero, para abreviar, llámame Juan. Empecé en la programación en 2018, cuando vi
+        que este mundillo tenía infinidad de salidas laborales.
+      </p>
+
+      <p>Actualmente estoy buscando empleo de FRONT DEVELOPER.</p>
+
+      <p>
+        Tengo un año de experiencia desarrollando apps en <span id='react'>ReactJS</span>. En primer lugar, desarrollé
+        apps relacionadas con el sector de la climatología, <span id='html'>HTML</span>, y <span id='css'>CSS</span>.
+        Además utilicé APIs externas y propias. Tiempo después estuve desarrollando una app que implementaba el concepto
+        de identidad digital, también con <span id='react'>ReactJS</span>.
+      </p>
+
+      <p>
+        En primer lugar, desarrollé apps relacionadas con el sector de la climatología, incluyendo{' '}
+        <span id='html'>HTML</span>, y <span id='css'>CSS</span>. Además utilicé APIs externas y propias. Tiempo después
+        estuve desarrollando una app que implementaba el concepto de identidad digital, también con{' '}
+        <span id='react'>ReactJS</span>.
+      </p>
+      <p>
+        Además, también estoy ampliando mis conocimientos con <span id='vue'>VueJS</span> y{' '}
+        <span id='angular'>AngularJS</span>.
+      </p>
     </div>
   );
 }
