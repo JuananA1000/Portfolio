69ee5da049513b91eebed4cb988396c2dba6ea86
Juanan <juanantonioamillopez@gmail.com>
2024-04-03 12:40:59 +0200

    prevision de foto

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,6 +1,8 @@
 import translate from '../../es.json';
 import about from '../../images/svg/about.svg';
 
+import photo from '../../images/img/calcCoctel.png';
+
 import './About.css';
 
 export default function About() {
@@ -11,30 +13,36 @@ export default function About() {
         <h2>{translate['about-h2']}</h2>
       </div>
 
-      <p>
-        Me llamo Juan Antonio pero, para abreviar, llámame Juan. Empecé en la programación en 2018, cuando vi
-        que este mundillo tenía infinidad de salidas laborales.
-      </p>
+      <div className='sobre-mi'>
+        <div className='descripcion'>
+          <p>
+            Me llamo Juan Antonio pero, para abreviar, llámame Juan. Empecé en la programación en 2018, cuando vi que
+            este mundillo tenía infinidad de salidas laborales.
+          </p>
 
-      <p>Actualmente estoy buscando empleo de FRONT DEVELOPER.</p>
+          <p>Actualmente estoy buscando empleo de FRONT DEVELOPER.</p>
 
-      <p>
-        Tengo un año de experiencia desarrollando apps en <span id='react'>ReactJS</span>. En primer lugar, desarrollé
-        apps relacionadas con el sector de la climatología, <span id='html'>HTML</span>, y <span id='css'>CSS</span>.
-        Además utilicé APIs externas y propias. Tiempo después estuve desarrollando una app que implementaba el concepto
-        de identidad digital, también con <span id='react'>ReactJS</span>.
-      </p>
+          <p>
+            Tengo un año de experiencia desarrollando apps en <span id='react'>ReactJS</span>. En primer lugar,
+            desarrollé apps relacionadas con el sector de la climatología, <span id='html'>HTML</span>, y{' '}
+            <span id='css'>CSS</span>. Además utilicé APIs externas y propias. Tiempo después estuve desarrollando una
+            app que implementaba el concepto de identidad digital, también con <span id='react'>ReactJS</span>.
+          </p>
 
-      <p>
-        En primer lugar, desarrollé apps relacionadas con el sector de la climatología, incluyendo{' '}
-        <span id='html'>HTML</span>, y <span id='css'>CSS</span>. Además utilicé APIs externas y propias. Tiempo después
-        estuve desarrollando una app que implementaba el concepto de identidad digital, también con{' '}
-        <span id='react'>ReactJS</span>.
-      </p>
-      <p>
-        Además, también estoy ampliando mis conocimientos con <span id='vue'>VueJS</span> y{' '}
-        <span id='angular'>AngularJS</span>.
-      </p>
+          <p>
+            En primer lugar, desarrollé apps relacionadas con el sector de la climatología, incluyendo{' '}
+            <span id='html'>HTML</span>, y <span id='css'>CSS</span>. Además utilicé APIs externas y propias. Tiempo
+            después estuve desarrollando una app que implementaba el concepto de identidad digital, también con{' '}
+            <span id='react'>ReactJS</span>.
+          </p>
+          <p>
+            Además, también estoy ampliando mis conocimientos con <span id='vue'>VueJS</span> y{' '}
+            <span id='angular'>AngularJS</span>.
+          </p>
+        </div>
+
+        <img src={photo} alt='foto-prevista.jpg' width='auto' height='auto' />
+      </div>
     </div>
   );
 }
