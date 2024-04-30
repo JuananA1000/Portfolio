379b9b37ee1ec700705a58c3899539bcab72294a
Juanan <juanantonioamillopez@gmail.com>
2024-04-30 11:23:53 +0200

    Aviso de e-mail copiado

--- src/components/Mailbutton/Mailbutton.jsx
+++ src/components/Mailbutton/Mailbutton.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import copyIcon from '../../images/svg/copy.svg';
 
 import translate from '../../es.json';
@@ -7,13 +9,24 @@ import { MAIL_LINK } from '../../constants/links';
 import './Mailbutton.css';
 
 export default function MailButton() {
+  const [clicked, setClicked] = useState(false);
+
+  function copyMail() {
+    navigator.clipboard.writeText(MAIL_LINK);
+    setClicked(true);
+    setTimeout(() => setClicked(false), 1000);
+  }
+
   return (
-    <div
-      className='mail'
-      onClick={() => navigator.clipboard.writeText(MAIL_LINK)}
-      title={translate['description-button-copypaste']}>
-      <img src={copyIcon} className='copy-icon' width={15} />
-      {MAIL_LINK}
+    <div className='mail-container'>
+      <div
+        className={`${clicked ? 'mail-copied' : 'mail'}`}
+        onClick={copyMail}
+        title={translate['description-button-copypaste']}>
+        <img src={copyIcon} className='copy-icon' width={15} />
+        {MAIL_LINK}
+      </div>
+      <div className='copy-alert'>{clicked ? translate['description-button-copied'] : ''}</div>
     </div>
   );
 }
