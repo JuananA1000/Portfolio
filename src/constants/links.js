d87a3038d9b3b593459a054e234ca7a6c72a8a84
Juanan <juanantonioamillopez@gmail.com>
2025-06-02 08:41:35 +0200

    link actualizado

--- src/constants/links.js
+++ src/constants/links.js
@@ -11,7 +11,7 @@ export const FRUTEREACT_CODE_LINK = 'https://github.com/JuananA1000/FruteReact';
 
 // ¡OJO! Este link, a veces, cambia. Si la imagen desaparece, volveremos a copiarlo del perfil de linkedin: https://www.linkedin.com/in/juan-antonio-a1000-lopez/
 export const FOTO_PERFIL_LINK =
-  'https://media.licdn.com/dms/image/v2/D4D03AQHGPc3SfjuGqw/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1710837838545?e=1741219200&v=beta&t=ymMLQj1j8fmakftpKFFRlpERCDXadZuTtINZEtaRIts';
+  'https://media.licdn.com/dms/image/v2/D4D03AQHGPc3SfjuGqw/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1710837838545?e=1754524800&v=beta&t=djdIxoWGp1Xarslw3KfxEsvir1A3eugV1O1AVEJYQAs';
 
 export const REACTYPE_LINK = 'https://reactype-app.netlify.app/';
 export const REACTYPE_CODE_LINK = 'https://github.com/JuananA1000/ReacType';
