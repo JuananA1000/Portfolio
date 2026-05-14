23dbc9c134a2814713e16e4d02f55bfdecaf5af4
Juanan <juanantonioamillopez@gmail.com>
2026-05-14 10:07:28 +0200

    FIX: Botón a mano

--- src/components/Buttons/Button.jsx
+++ src/components/Buttons/Button.jsx
@@ -2,7 +2,7 @@ import './Button.css';
 
 export default function Button({ children, link, icon }) {
   return (
-    <a className='button' href={link} target='blank'>
+    <a className='chalk-btn' href={link} target='blank'>
       {icon && <img src={icon} alt='Icon' className='button-icon' width={15} />}
       {children}
     </a>
